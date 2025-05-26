1f45ceb93684f3b7b1d93ce0a020fb160be78062
Hajibayli Ismayil Fuad <hajibayli_i@itstep.edu.az>
2025-05-26 12:03:01 +0400

    add header markup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const links = ["Home", "About", "Contact", "Products"];
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const header = document.querySelector("header");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const nav = document.createElement("nav");
+
+const menu = links
+  .map((link) => {
+    return `<a>${link}</a>`;
+  })
+  .join("");
+nav.innerHTML = menu;
+header?.append(nav);
